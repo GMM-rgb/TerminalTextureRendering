ad8b7d7dd6ee615707e06297c961103ad5d3cf2c
Maximus Farvour <6041464@luhsd.net>
2026-05-15 09:59:31 -0700

    Initial commit for command line texture rendering, library utility framework.

---
+++ source/modules/texture_importing.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+namespace TexturePlainImporting {
+    inline auto ImportTextureFile(const std::string TextureFileLocation) {
+
+    }
+}
---
+++ source/texture_core.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <regex>
+
+#include "./headers/TEXTURE_IMPORTS.hpp"
+
+int main() {
+    
+}
